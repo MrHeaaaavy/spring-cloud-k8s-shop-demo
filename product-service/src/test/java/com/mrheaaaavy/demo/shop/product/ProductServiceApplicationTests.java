f7b066181eb022d5173b412170ee3af45226aaae
杨振彪 <yangzhenbiao@gdmcmc.cn>
2020-07-17 16:49:09 +0800

    Init

---
+++ product-service/src/test/java/com/mrheaaaavy/demo/shop/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrheaaaavy.demo.shop.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
