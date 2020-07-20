706d0b92379ecb766fb50994c94a6fe2b0952ad8
杨振彪 <yangzhenbiao@gdmcmc.cn>
2020-07-20 19:08:57 +0800

    Docker push.

--- member-service/src/main/java/com/mrheaaaavy/demo/shop/member/handler/MemberHandler.java
+++ member-service/src/main/java/com/mrheaaaavy/demo/shop/member/handler/MemberHandler.java
@@ -2,12 +2,19 @@ package com.mrheaaaavy.demo.shop.member.handler;
 
 import com.mrheaaaavy.demo.shop.member.response.Member;
 import com.mrheaaaavy.demo.shop.member.response.MemberDetailResponse;
+import com.mrheaaaavy.demo.shop.member.response.MemberListResponse;
 import com.mrheaaaavy.demo.shop.trade.client.TradeClient;
 import com.mrheaaaavy.demo.shop.trade.response.TradeListResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mrheaaaavy
+ */
 @RestController
 @RequestMapping("/members")
 public class MemberHandler {
@@ -27,4 +34,19 @@ public class MemberHandler {
                 .trades(tradeListResponse.getTrades())
                 .build();
     }
+
+    @GetMapping("")
+    public MemberListResponse list() {
+        List<Member> members = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            members.add(new Member(String.format("member#%d", i)));
+        }
+
+        return new MemberListResponse()
+                .setMembers(members)
+                .setPage(0)
+                .setSize(0)
+                .setTotal(0);
+    }
+
 }
