a82ac4d24c5f0422e45c7f9fda83fd4499ede4c8
杨振彪 <yangzhenbiao@gdmcmc.cn>
2020-07-24 09:44:01 +0800

    Put all deploy resources together.

--- trade-service/src/main/java/com/mrheaaaavy/demo/shop/trade/handler/TradeHandler.java
+++ trade-service/src/main/java/com/mrheaaaavy/demo/shop/trade/handler/TradeHandler.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Function;
 
 
